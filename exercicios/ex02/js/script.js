efc87ae52835b16a58937e76be3dd83a12aac04c
Caio <142021038+C4IO-Dev@users.noreply.github.com>
2023-10-27 17:26:27 -0300

    criei ex 02

---
+++ exercicios/ex02/js/script.js
@@ -0,0 +1,9 @@
+function verificar(){
+    var ano = window.document.getElementById('ano')
+    var sexoescolhido = document.getElementById("sexo")
+    var sexo = sexoescolhido.value
+
+    var res = document.getElementById("resultado")
+
+    
+}
