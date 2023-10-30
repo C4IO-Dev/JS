3fb5202a4efd159e757a0f2e0ffe389b12e18f22
Caio <142021038+C4IO-Dev@users.noreply.github.com>
2023-10-30 14:00:49 -0300

    estruturas de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+//repetiçao com condição inicial
+
+/*
+while (c < 100000) {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} */
+
+var c = 1
+
+//repetição com condição no final
+
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 10);
