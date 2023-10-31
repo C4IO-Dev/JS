bb65d0d47e824928ba1bcbc2ad789585825d1aa8
Caio <142021038+C4IO-Dev@users.noreply.github.com>
2023-10-31 19:44:47 -0300

    array

---
+++ aula15/ambiente.js
@@ -0,0 +1,13 @@
+//array
+let num = [19, 29, 40]
+
+//adicionei o número 6 a posição 3 da array
+num [3] = 6
+
+//adicionei o número 7 ao final da array
+num.push(7)
+
+//quantia de elementos presentes na array
+console.log(num.length)
+
+console.log(`Nosso Vetor é ${num}`)
